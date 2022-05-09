58875fbd2d15781bdd59d5811928f103af814445
hh_nsul@outlook.com <hh_nsul@outlook.com>
2022-05-09 22:52:26 +0800

    Project initial structure built

---
+++ restaurant-frontend/src/test/java/com/jet/restaurant/frontend/RestaurantFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jet.restaurant.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ restaurant-state-management/src/main/java/com/jet/restaurant/state_management/RestaurantStateManagementApplication.java
@@ -0,0 +1,13 @@
+package com.jet.restaurant.state_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantStateManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantStateManagementApplication.class, args);
+    }
+
+}
